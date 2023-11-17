ae7c17c18a78122ba07932cf0aff352596c96587
kieron <kieronboz@gmail.com>
2023-11-17 14:06:46 +0000

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>CSV to PDF Converter</h1>
+    <FileUpload />
+  </div>
+</template>
+
+<script>
+import FileUpload from '@/components/FileUpload.vue';
+
+export default {
+  components: {
+    FileUpload,
+  },
+};
+</script>
